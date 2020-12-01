be6db3a080b566efd225ea2324f47582f43ac82a
team9-hcmus <75234933+team9-hcmus@users.noreply.github.com>
2020-12-01 21:23:53 +0700

    Create c++.cpp

---
+++ c++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float ki, mi;
+	cout << "enter value in kilometers" << endl;
+	cin >> ki;
+	mi = ki * 0.62137;
+	cout << "your value in miles" << mi;
+	return(0);
+}
